7819d3f434aef2e840851585fc0b51064029d1df
bamamaulanaw <bamamaulanaw@gmail.com>
2026-01-11 13:33:48 +0700

    Setting routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Halaman Utama
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+
+// Simpan Tugas
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+// Tandai Selesai (Update)
+Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+// Hapus Tugas
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+?>
